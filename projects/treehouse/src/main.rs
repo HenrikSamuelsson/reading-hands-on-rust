1428f531ab6458146ee6c7bd8f08a59a14607397
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-06-28 20:36:20 +0200

    Format code with Rust-Analyzer,

--- projects/treehouse/src/main.rs
+++ projects/treehouse/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     println!("Hello, what's your name?");
     let mut your_name = String::new();
     use std::io::stdin;
-    stdin().read_line(&mut your_name).expect("Failed to read line");
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
     println!("Hello, {}", your_name)
 }
