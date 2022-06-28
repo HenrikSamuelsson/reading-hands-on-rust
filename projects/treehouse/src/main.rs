92a2941cf50f6c34ce5ee1b88707df109ee2edb3
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-06-28 20:34:52 +0200

    Add read in and print of name.

--- projects/treehouse/src/main.rs
+++ projects/treehouse/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, what's your name?");
+    let mut your_name = String::new();
+    use std::io::stdin;
+    stdin().read_line(&mut your_name).expect("Failed to read line");
+    println!("Hello, {}", your_name)
 }
