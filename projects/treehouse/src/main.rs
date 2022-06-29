2bd0c8447fb7f66c1f0b4cc20456e3a09c350831
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-06-29 19:55:25 +0200

    Break parts out into a function.

--- projects/treehouse/src/main.rs
+++ projects/treehouse/src/main.rs
@@ -1,9 +1,15 @@
-fn main() {
-    println!("Hello, what's your name?");
+use std::io::stdin;
+
+fn what_is_your_name() -> String {
     let mut your_name = String::new();
-    use std::io::stdin;
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    println!("Hello, {}", your_name)
+    your_name
+}
+
+fn main() {
+    println!("Hello, what's your name?");
+    let name = what_is_your_name();
+    println!("Hello, {}", name)
 }
