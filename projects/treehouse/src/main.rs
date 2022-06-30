dc55e2e139876fbaecba6124e0fd26317b0cba38
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-06-30 18:22:09 +0200

    Add debug print.

--- projects/treehouse/src/main.rs
+++ projects/treehouse/src/main.rs
@@ -11,5 +11,5 @@ fn what_is_your_name() -> String {
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {}", name)
+    println!("{:?}", name)
 }
